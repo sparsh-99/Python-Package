f4e15277a977d96464672ac1d942231427ed3cb7
Sparsh Garg <sparsh.garg2019@outlook.com>
2020-06-04 03:47:55 +0530

    Update OOPS_Exercise_1.py

--- Exercise_1/OOPS_Exercise_1.py
+++ Exercise_1/OOPS_Exercise_1.py
@@ -3,18 +3,8 @@
 
 # # Use the Shirt Class
 # 
-# You've seen what a class looks like and how to instantiate an object. Now it's your turn to write code that insantiates a shirt object.
-# 
-# # Explanation of the Code
-# This Jupyter notebook is inside of a folder called 1.OOP_syntax_shirt_practice. You can see the folder if you click on the "Jupyter" logo above the notebook. Inside the folder are three files:
-# - shirt_exercise.ipynb, which is the file you are currently looking at
-# - answer.py containing answers to the exercise
-# - tests.py, tests for checking your code - you can run these tests using the last code cell at the bottom of this notebook
-# 
 # # Your Task
 # The shirt_exercise.ipynb file, which you are currently looking at if you are reading this, has an exercise to help guide you through coding with an object in Python.
-# 
-# Fill out the TODOs in each section of the Jupyter notebook. You can find a solution in the answer.py file.
 # 
 # First, run this code cell below to load the Shirt class.
 
