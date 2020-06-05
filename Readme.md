00be2612b950cbc60af9c6eaf2d355745e0e7ebe
Sparsh Garg <sparsh.garg2019@outlook.com>
2020-06-06 04:52:52 +0530

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,10 @@
+# gabi-distribution package
+gabi stands for Gaussian-Binomial Distribution python package
+
+# Files
+Explanation of files in the package
+
+# Installation
+pip install gabi-probability
+
+# Created by Sparsh Garg
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gabi_probability',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['gabi_probability'],
+      author="Sparsh Garg",
+      author_email="sparsh.garg2019@outlook.com",
+      zip_safe=False)
